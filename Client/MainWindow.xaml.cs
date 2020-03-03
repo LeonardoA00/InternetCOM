e15bd4fde703b8b4935bd1b031c4636716c092ee
LeonardoA00 <58164719+LeonardoA00@users.noreply.github.com>
2020-03-03 13:41:48 +0100

    Client-Server C# App

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        const string ServerIP = "127.0.0.1";
+        const int port = 8888;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            servermsg("Client Started");
+            try 
+            { 
+                clientSocket.Connect(ServerIP, port);
+                ConnectionStatus.Content = "Client Socket Program - Server Connected ...";
+            } 
+            catch (Exception ex)
+            {
+                ConnectionStatus.Content = "Unable to connecto to the server... Restart the app";
+            }
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(ClientTextBox.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            servermsg(returndata);
+            ClientTextBox.Text = "";
+            ClientTextBox.Focus();
+        }
+
+        public void servermsg(string mesg)
+        {
+            ServerTextBox.Text = ServerTextBox.Text + Environment.NewLine + " >> " + mesg;
+        }
+    }
+}
---
+++ InternetCom/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+
+        const int port = 8888;
+
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(port);
+            serverSocket.Start();
+            Console.WriteLine("...Server started");
+            TcpClient clientSocket = default(TcpClient);
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("...Accept connection from client");
+
+            int requestCount = 0;
+            while(true)
+            {
+                try
+                {
+                    requestCount++;
+                    NetworkStream networkStream = clientSocket.GetStream();
+
+                    byte[] bytesFrom = new byte[10025];
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine($">> {dataFromClient}");
+
+                    string serverResponse = "Recieved request " + requestCount.ToString();
+                    byte[] sendBytes = System.Text.Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine($"--> {serverResponse}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    break;
+                }
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(">> Server stopped");
+            Console.ReadLine();
+        }
+    }
+}
